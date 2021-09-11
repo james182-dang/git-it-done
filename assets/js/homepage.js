78f9dddcbd3a10e7109b72ba0213f51c190a3e20
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-09-11 19:15:43 -0400

    add language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -78,6 +78,20 @@ var displayRepos = function(repos, searchTerm) {
     };
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
+
 // acquire repos from github API function
 var getUserRepos = function(user) {
     // format the github api url
