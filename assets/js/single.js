1bd75b2a6176fa5f9308a85f599b9e0a07d3b32e
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-09-11 17:57:35 -0400

    add issue overflow handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,19 @@
 // DOM elements
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "the Issues page on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -10,6 +24,11 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to DOM function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -54,4 +73,4 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("james182-dang/run-buddy");
+getRepoIssues("facebook/react");
