0c8931c5bb1f5489c3a8747a0bc42aaf0ed0944e
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-09-11 18:54:03 -0400

    add issues functionality and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,7 @@
 // DOM elements
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var displayWarning = function(repo) {
     // add text to warning container
@@ -15,9 +16,28 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
+// get query string from homepage.js
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
@@ -31,7 +51,8 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -73,4 +94,4 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
